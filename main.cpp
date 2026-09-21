3fbc17df8ecd9f819983f1ab78477eb2969fc5bf
Nora Jordan <njordan5@cub.uca.edu>
2026-09-21 15:42:48 -0500

    Added member discount logic and audit table
    ^_^

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@ int main()
 	string foodName;
 	char itemCode;
 	int itemQuantity;
+	double discount = 0.1;
 	double unitPrice;
 	bool isMember;
+	string cashierNotes;
 
 	cout << "Enter in a food name: " << endl;
 	cin >> foodName;
@@ -24,14 +26,26 @@ int main()
 	cout << "\n" << setw(10) << left << "Receipt\n ------------ \n"
 		<< "Name: " << foodName << "\n"
 		<< "Item Code: " << itemCode << "\n"
+		<< "Item Quantity: " << itemQuantity << "\n";
+	if (isMember) 
+	{
+		cout << "Membership: Yes " << "\n";
+		unitPrice = unitPrice * (1 - discount);
+	}
+	else
+	{
+		cout << "Membership: No " << "\n";
+	}
+	cout << "Unit Price: " << right << fixed << setprecision(2) << "$" << unitPrice << "\n";
+	
+	cout << "\n"
+	    << "Enter Cashier Notes: ";
+	cin >> cashierNotes;
+	
+	cout << "\n" << setw(10) << left << "Audit\n ------------ \n"
+		<< "Name: " << foodName << "\n"
+		<< "Item Code: " << itemCode << "\n"
 		<< "Item Quantity: " << itemQuantity << "\n"
-		<< "Unit Price: " << right  << fixed << setprecision(2) << "$" << unitPrice << "\n" << left;
-		if (isMember) 
-		{
-			cout << "Membership: No " << "\n";
-		}
-		else
-		{
-			cout << "Membership: Yes " << "\n";
-		}
+		<< "Unit Price: " << right << fixed << setprecision(2) << "$" << unitPrice << "\n" << left
+		<< "Discount: " << "$" << discount;
 }
