f0e4746b3e8ee43b5b0f9a36bd360950f5d40f20
Kelometer <km2.academic@gmail.com>
2026-09-21 18:54:03 -0500

    Dealt with some errors and fixed the formatting issues. Nice and pretty now :3

--- main.cpp
+++ main.cpp
@@ -1,51 +1,61 @@
 #include <iostream>
+#include <string>
 #include <iomanip>
+
 using namespace std;
 
 int main()
 {
 	string foodName;
+	string cashierNotes;
 	char itemCode;
 	int itemQuantity;
 	double discount = 0.1;
 	double unitPrice;
 	bool isMember;
-	string cashierNotes;
 
 	cout << "Enter in a food name: " << endl;
-	cin >> foodName;
+	getline(cin, foodName);
 	cout << "Enter in an item code (one character only): " << endl;
 	cin >> itemCode;
 	cout << "Enter in a quantity: " << endl;
 	cin >> itemQuantity;
 	cout << "Enter a price for the item: " << endl;
 	cin >> unitPrice;
-	cout << "Is the user a member or not? (1/0)" << endl;
+	cout << "Membership Active: (1/0)" << endl;
 	cin >> isMember;
 
-	cout << "\n" << setw(10) << left << "Receipt\n ------------ \n"
-		<< "Name: " << foodName << "\n"
-		<< "Item Code: " << itemCode << "\n"
-		<< "Item Quantity: " << itemQuantity << "\n";
-	if (isMember) 
-	{
-		cout << "Membership: Yes " << "\n";
+	cout << "\n" << "Receipt\n------------\n"
+		<< left << setw(15) << "Name: " << foodName << "\n"
+		<< left << setw(15) << "Item Code: " << itemCode << "\n"
+		<< left << setw(15) << "Item Quantity: " << itemQuantity << "\n";
+
+	if (isMember) {
+		cout << left << setw(15) << "Membership" << "Yes" << "\n";
 		unitPrice = unitPrice * (1 - discount);
 	}
-	else
-	{
-		cout << "Membership: No " << "\n";
+	else {
+		cout << left << setw(15) << "Membership" << "No" << "\n";
 	}
-	cout << "Unit Price: " << right << fixed << setprecision(2) << "$" << unitPrice << "\n";
-	
-	cout << "\n"
-	    << "Enter Cashier Notes: ";
-	cin >> cashierNotes;
-	
-	cout << "\n" << setw(10) << left << "Audit\n ------------ \n"
-		<< "Name: " << foodName << "\n"
-		<< "Item Code: " << itemCode << "\n"
-		<< "Item Quantity: " << itemQuantity << "\n"
-		<< "Unit Price: " << right << fixed << setprecision(2) << "$" << unitPrice << "\n" << left
-		<< "Discount: " << "$" << discount;
+
+	cout << left << setw(15) << "Unit Price:" << "$" << fixed << setprecision(2) << unitPrice << "\n";
+
+	cout << "\nEnter Cashier Notes: ";
+	cin.ignore();
+	getline(cin, cashierNotes);
+	cout << "\n" << left << "Audit\n------------" << endl;
+
+	cout << left << setw(15) << "Name"
+		<< setw(15) << "Item"
+		<< setw(15) << "Quantity"
+		<< setw(15) << "Unit Price"
+		<< setw(15) << "Discount\n" << endl;
+
+	cout << left << setw(15) << foodName
+		<< setw(15) << itemCode
+		<< setw(15) << itemQuantity
+		<< "$" << setw(14) << fixed << setprecision(2) << unitPrice
+		<< setw(15) << (isMember ? "Yes" : "No") << "\n";
+
+	return 0;
 }
